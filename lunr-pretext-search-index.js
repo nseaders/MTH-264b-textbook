fb0de82aa940cd0241e5203d09c6c20979672dd4
nseaders <132086655+nseaders@users.noreply.github.com>
2023-10-03 22:59:41 +0000

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,220 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "colophon-1",
+  "level": "1",
+  "url": "colophon-1.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "   linnbenton.edu   https:\/\/www.linnbenton.edu\/   copyright  "
+},
+{
+  "id": "examples",
+  "level": "1",
+  "url": "examples.html",
+  "type": "Section",
+  "number": "0.1",
+  "title": "Samples of various pretext formats",
+  "body": " Samples of various pretext formats     C    D       Does this make a difference?     Here is a paragraph. With quotations and single quotes , emphasis and angles .  Here are examples of the OscarLevin css style.     Here is a part of an ordered list    next part      try again       Here is an itemized list      Here is an itemized list            integral   An integral is a sum.     integral   An integral is a sum.     integral   An integral is a sum.     integral  An integral is a sum.    integral  An integral is a sum.   integral An integral is a sum. A variable is a container for information.   How to do an aside  here is an aside. can you footnote an aside?   with two paragraphs and    It seems that asides get lost a little on the right, so I think I should use them sparingly. How in the heck do you write a paragraph that is readable?  Exercise What  Does this work at all?    What happens if I do this? Blah blah blah     C    D      blah blah   "
+},
+{
+  "id": "objectives-1",
+  "level": "2",
+  "url": "examples.html#objectives-1",
+  "type": "Objectives",
+  "number": "0.1",
+  "title": "",
+  "body": "   C    D    "
+},
+{
+  "id": "investigation-1",
+  "level": "2",
+  "url": "examples.html#investigation-1",
+  "type": "Investigation",
+  "number": "0.1",
+  "title": "",
+  "body": " Does this make a difference?  "
+},
+{
+  "id": "objectives-2",
+  "level": "2",
+  "url": "examples.html#objectives-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "p-12",
+  "level": "2",
+  "url": "examples.html#p-12",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "integral integral integral integral integral integral variable "
+},
+{
+  "id": "exercise-2",
+  "level": "2",
+  "url": "examples.html#exercise-2",
+  "type": "You Try",
+  "number": "0.4",
+  "title": "Exercise What.",
+  "body": "Exercise What  Does this work at all?  "
+},
+{
+  "id": "outcomes-1",
+  "level": "2",
+  "url": "examples.html#outcomes-1",
+  "type": "Outcomes",
+  "number": "0.1",
+  "title": "",
+  "body": "   C    D    "
+},
+{
+  "id": "exercise-3",
+  "level": "2",
+  "url": "examples.html#exercise-3",
+  "type": "Reading Question",
+  "number": "1",
+  "title": "",
+  "body": "blah blah "
+},
+{
+  "id": "section-variables",
+  "level": "1",
+  "url": "section-variables.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Variables",
+  "body": " Variables  In this section we will learn how to assign values to a variable in Python and display results.     Create a variable in Python using =     Display a variable in Python using print()      A variable is like a container that can be used to store information or data. In Python you assign a variable using = .  Run the Python code below by clicking the Evaluate (Python) button.   Note that the function print() was needed to display the value of the variable.  Change the value of the variable a by adding 5 after the 2*3 above and run the code again to see how the output changes.   Naming Variables  Variables can be named using letters, underscores, numbers, etc. Just be sure to use the exact same name to call your variable. For example, the following code has a syntax error.  Run the code as is and see what happens.    Can you fix the bug in the above code so it runs correctly?   Variables are case sensitive and must be exactly the same to be recognized.     Arithmetic and Comments in Python  Python recognizes simple arithmetic symbols like + -, *, \/ .  Meanwhile a hashtag # can be used to tell Python to ignore code, reading what follows as a comment by the author rather than a Python command.   Comments can help anyone reading your code understand what that section of the code is doing. You can also comment out portions of code to isolate a bug.  In the box below, change the value of the variable arithmetic to try out some basic calculations. You can use the standard order of operations and parentheses (PEMDAS).     More about Variables  Just like in math, variables can be helpful when the information will change. Recall the formula for the area of a circle .  In the code below,    fill in the formula for the area of the circle using the appropriate variable names and arithmetic operations.      Use the code to approximately compute the area of a circle with radius 5 and a circle with radius 13.       Syntax errors vs. Symantic errors   Say stuff here about errors - blah blah blah.   Summary     A variable can be used as a container to store data or information.      Use print(variable_name) to display the information stored in the variable.      Use # to comment and make your code more readable.       test it out   "
+},
+{
+  "id": "objectives-3",
+  "level": "2",
+  "url": "section-variables.html#objectives-3",
+  "type": "Objectives",
+  "number": "1.1",
+  "title": "",
+  "body": "   Create a variable in Python using =     Display a variable in Python using print()     "
+},
+{
+  "id": "p-31",
+  "level": "2",
+  "url": "section-variables.html#p-31",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "variable "
+},
+{
+  "id": "exercise-4",
+  "level": "2",
+  "url": "section-variables.html#exercise-4",
+  "type": "You Try",
+  "number": "1.1",
+  "title": "",
+  "body": "Change the value of the variable a by adding 5 after the 2*3 above and run the code again to see how the output changes. "
+},
+{
+  "id": "exercise-5",
+  "level": "2",
+  "url": "section-variables.html#exercise-5",
+  "type": "You Try",
+  "number": "1.2",
+  "title": "",
+  "body": " Can you fix the bug in the above code so it runs correctly?   Variables are case sensitive and must be exactly the same to be recognized.  "
+},
+{
+  "id": "exercise-6",
+  "level": "2",
+  "url": "section-variables.html#exercise-6",
+  "type": "Exercise",
+  "number": "1",
+  "title": "",
+  "body": "test it out "
+},
+{
+  "id": "section-modules",
+  "level": "1",
+  "url": "section-modules.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Python Packages and NumPy",
+  "body": " Python Packages and NumPy  Python packages and libraries are collections of functions and modules centered around a common theme. (For example, NumPy is a scientific computing package.) In this section we will learn how to import a Python package and use functions from that package.     Import a Python package or library (our example NumPy).    Use functions from a Python package.     NumPy is an open source scientific computing package that allows you to use standard mathematical functions and constants like sine or pi. NumPy also allows you to work with arrays of numbers so you can efficiently perform computations.   Note to call a function from an imported package you must use the syntax package.function . To avoid retyping the long name of a package every time, it is standard practice to rename the package as you import.  Rename the package numpy in the above code by replacing the import line with import numpy as np . Next use the new name of the package to call the function np.sin() and re-run.  In addition to the standard trigonometric functions, NumPy also contains as exp() and natural log as log() , as well as the standard mathematical constants like pi and e .  Use the NumPy package to compute the area of a circle using pi. If you import numpy as np, you will use np.pi   NumPy arrays are an efficient way to do computations with a list of numbers. You can also represent vectors or matrices using NumPy arrays. Run the code below to see how it works.   You can find more information about the package NumPy at . Python's wiki has a list of other commonly used modules and packages .   Debugging revisited  Add another exercise that they need to debug.    Summary     You can import a Python package and rename it by using import package as name .      You can call a function from a package using packagename.function       "
+},
+{
+  "id": "objectives-4",
+  "level": "2",
+  "url": "section-modules.html#objectives-4",
+  "type": "Objectives",
+  "number": "1.2",
+  "title": "",
+  "body": "   Import a Python package or library (our example NumPy).    Use functions from a Python package.    "
+},
+{
+  "id": "exercise-7",
+  "level": "2",
+  "url": "section-modules.html#exercise-7",
+  "type": "You Try",
+  "number": "1.3",
+  "title": "",
+  "body": "Rename the package numpy in the above code by replacing the import line with import numpy as np . Next use the new name of the package to call the function np.sin() and re-run. "
+},
+{
+  "id": "exercise-8",
+  "level": "2",
+  "url": "section-modules.html#exercise-8",
+  "type": "You Try",
+  "number": "1.4",
+  "title": "",
+  "body": "Use the NumPy package to compute the area of a circle using pi. If you import numpy as np, you will use np.pi "
+},
+{
+  "id": "section-modules",
+  "level": "1",
+  "url": "section-modules.html",
+  "type": "Section",
+  "number": "1.3",
+  "title": "MatPlotLib and 2D graphing",
+  "body": " MatPlotLib and 2D graphing  In this section we will learn how to use the library MatPlotLib to graph in 2D.     C    D     what    Basic Graphs     what went wrong? It just always connects the dots...    Axes and Labels    Summary     blah      blah      "
+},
+{
+  "id": "objectives-5",
+  "level": "2",
+  "url": "section-modules.html#objectives-5",
+  "type": "Objectives",
+  "number": "1.3",
+  "title": "",
+  "body": "   C    D    "
+},
+{
+  "id": "section3-2",
+  "level": "1",
+  "url": "section3-2.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "And so it begins...",
+  "body": " And so it begins...   Here is an introduction   "
+},
+{
+  "id": "colophon-2",
+  "level": "1",
+  "url": "colophon-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
